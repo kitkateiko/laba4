1b70f35ce47afc22e02af49b743ba7d993c094b1
kitkateiko <tyrkov2003@mail.ru>
2024-10-18 20:46:23 +0700

    3

--- app/src/main/java/com/example/laba4/MainActivity.kt
+++ app/src/main/java/com/example/laba4/MainActivity.kt
@@ -40,6 +40,10 @@ class MainActivity : AppCompatActivity() {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
         }
+        questionTextView.setOnClickListener(){
+            currentIndex = (currentIndex + 1) % questionBank.size
+            updateQuestion()
+        }
 
         updateQuestion()
 
