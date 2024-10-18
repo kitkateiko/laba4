a2ff053a19e3fc3d398d676d053464d5debf1092
kitkateiko <tyrkov2003@mail.ru>
2024-10-18 20:27:23 +0700

    3

--- app/src/main/java/com/example/laba4/MainActivity.kt
+++ app/src/main/java/com/example/laba4/MainActivity.kt
@@ -32,11 +32,10 @@ class MainActivity : AppCompatActivity() {
         nextButton = findViewById(R.id.next_button)
         questionTextView = findViewById(R.id.question_text_view)
         trueButton.setOnClickListener { view: View ->
-            Toast.makeText(this,R.string.correct_toast,Toast.LENGTH_SHORT).show()
+            checkAnswer(true)
         }
         falseButton.setOnClickListener { view: View ->
-            Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show()
-        }
+            checkAnswer(false)        }
         nextButton.setOnClickListener {
             currentIndex = (currentIndex + 1) % questionBank.size
             updateQuestion()
