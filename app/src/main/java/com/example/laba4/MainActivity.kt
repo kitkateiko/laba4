665c15cf04aa7ac69ca65fef02b7a05945c0e8d0
kitkateiko <tyrkov2003@mail.ru>
2024-10-18 19:38:01 +0700

    1

--- app/src/main/java/com/example/laba4/MainActivity.kt
+++ app/src/main/java/com/example/laba4/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.example.laba4
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button;
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener { view: View ->
+// Что-то выполнить после нажатия
         }
+        falseButton.setOnClickListener { view: View ->
+// Что-то выполнить после нажатия
+        }
+
+
+            //    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+          //  val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+         //   v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+          //  insets
+        //}
     }
 }
