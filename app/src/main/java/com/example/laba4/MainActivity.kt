2292f33b8c6048e61781caa079c2daf49951346b
kitkateiko <tyrkov2003@mail.ru>
2024-10-18 19:42:27 +0700

    1

--- app/src/main/java/com/example/laba4/MainActivity.kt
+++ app/src/main/java/com/example/laba4/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this,R.string.incorrect_toast,Toast.LENGTH_SHORT).show()
         }
 
-
+//
             //    ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
           //  val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
          //   v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
